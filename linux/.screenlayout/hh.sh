fa4d221b4752cb125c3fb853cb2e1717ca9f4891
Collin Reilly Clark <crclark96@gmail.com>
2020-09-21 16:47:38 -0400

    add screenlayout scripts for thinkpad

---
+++ linux/.screenlayout/hh.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 3840x2160 --pos 3960x0 --rotate normal --output DP-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --scale 2x2 --output DP-2 --off
